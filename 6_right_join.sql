8923c97b45466e15a9b92a034ee34fed29b86499
ColeCz <124841853+ColeCz@users.noreply.github.com>
2024-11-03 01:19:53 -0500

    Update 6_right_join.sql

--- 6_right_join.sql
+++ 6_right_join.sql
@@ -1,22 +1,22 @@
-Table: Employees
-+---------------+---------+
-| Column Name   | Type    |
-+---------------+---------+
-| id            | int     |
-| name          | varchar |
-+---------------+---------+
+-- Table: Employees
+-- +---------------+---------+
+-- | Column Name   | Type    |
+-- +---------------+---------+
+-- | id            | int     |
+-- | name          | varchar |
+-- +---------------+---------+
 
  
-Table: EmployeeUNI
-+---------------+---------+
-| Column Name   | Type    |
-+---------------+---------+
-| id            | int     |
-| unique_id     | int     |
-+---------------+---------+
+-- Table: EmployeeUNI
+-- +---------------+---------+
+-- | Column Name   | Type    |
+-- +---------------+---------+
+-- | id            | int     |
+-- | unique_id     | int     |
+-- +---------------+---------+
 
 
-Write a solution to show the unique ID of each user, If a user does not have a unique ID replace just show null.
+-- Write a solution to show the unique ID of each user, If a user does not have a unique ID replace just show null.
 
 SELECT EmployeeUNI.unique_id, Employees.name
 FROM EmployeeUNI
